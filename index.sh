6d9bc5db38e1e021a07d36710b1ffbbeb627ce9f
Manfred Touron <94029+moul@users.noreply.github.com>
2020-07-08 16:26:15 +0200

    feat: install_tools & adduser

--- index.sh
+++ index.sh
@@ -29,12 +29,15 @@ Usage: curl -s https://sh.moul.io | sh -s -- <subcommand> [options]
 Subcommands:
     authorized_keys     add keys from github.com/moul.keys into .ssh/authorized_keys
     install_docker      use get.docker.com script to install docker
+    install_tools       install common tools (tmux, htop, git, ssh, curl, wget, mosh, emacs)
+    adduser             create a new moul user, install SSH keys, configure docker & sudo
 
 More info: https://github.com/moul/sh.moul.io
 EOF
 }
 
 sub_authorized_keys() {
+    # FIXME: support other $USER
     umask 077
     mkdir -p .ssh
     echo "" >> .ssh/authorized_keys
@@ -46,6 +49,24 @@ sub_authorized_keys() {
 sub_install_docker() {
     curl -fsSL https://get.docker.com -o get-docker.sh
     sh get-docker.sh
+    # check if docker-compose is available
+}
+
+sub_install_tools() {
+    # FIXME: support other distributions
+    sudo apt install tmux htop emacs-nox git ssh curl wget mosh
+}
+
+sub_adduser() {
+    # FIXME: support other $USER
+    useradd -m moul
+    usermod -aG docker moul
+    usermod --shell=/bin/bash moul
+    mkdir -p /home/moul/.ssh
+    umask 077
+    curl https://github.com/moul.keys >> /home/moul/.ssh/authorized_keys
+    chown -R moul:moul /home/moul/.ssh
+    echo "moul ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 }
 
 main() {
