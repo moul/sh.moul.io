da1f2709c26f68d2c581a81a90948921f03e546f
Manfred Touron <94029+moul@users.noreply.github.com>
2020-07-08 16:07:21 +0200

    feat: add authorized_keys

--- index.sh
+++ index.sh
@@ -1,5 +1,62 @@
 #!/bin/sh
 set -e
-## See https://github.com/moul/sh.moul.io
+if [ "x$TRACE" != "x" ]; then set -x; fi
 
-echo "hello world!"
+#       ++
+#       ++++
+#        ++++
+#      ++++++++++
+#     +++       |
+#     ++         |
+#     +  -==   ==|
+#    (   <*>   <*>
+#     |           |
+#     |         __|
+#     |      +++
+#      \      =+
+#       \      +                           more info here:
+#       |\++++++                  https://github.com/moul/sh.moul.io
+#       |  ++++      ||//
+#   ____|   |____   _||/__
+#  /     ---     \  \|  |||
+# /  _ _  _     / \   \ /
+# | / / //_//_//  |   | |
+
+sub_help() {
+    cat <<EOF
+Usage: $(basename $0) <subcommand> [options]
+
+Subcommands:
+    authorized_keys     add keys from github.com/moul.keys into .ssh/authorized_keys
+
+More info: https://github.com/moul/sh.moul.io
+EOF
+}
+
+sub_authorized_keys() {
+    umask 077
+    mkdir -p .ssh
+    echo "" >> .ssh/authorized_keys
+    echo "# https://github.com/moul.keys" >> .ssh/authorized_keys
+    curl https://github.com/moul.keys >> .ssh/authorized_keys
+    echo "" >> .ssh/authorized_keys
+}
+
+main() {
+    subcommand=$1
+    case $subcommand in
+        "" | "-h" | "--help")
+            sub_help
+            ;;
+        *)
+            shift
+            sub_${subcommand} $@
+            if [ $? = 127 ]; then
+                echo "Error: '$subcommand' is not a known subcommand." >&2
+                echo "       Run '$(basename $0) --help' for a list of known subcommands." >&2
+            fi
+            ;;
+    esac
+}
+
+main "$@"
